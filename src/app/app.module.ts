a33d576b45f4720f1285a9f38a14b4d73793f79c
Subham Das <sdas1@corp.miraclesoft.com>
2022-12-15 20:29:06 +0530

    add_update_data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './account/login/login.component';
+import { RegisterComponent } from './account/register/register.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './todo/Notfound.component';
 
+const routes:Routes =[
+{path:'',component:LoginComponent},
+{path:"LoginComponent",component:LoginComponent},
+{path :"register",component:RegisterComponent},
+{path:'home',component:HomeComponent,children:[
+  {
+    path:'',loadChildren:()=>import("./dashboard/dashboard.module").then((u)=>u.DashboardModule),
+  },
+  {
+    path:'todo',loadChildren:()=>import("./todo/todo.module").then((u)=>u.TodoModule),
+  }
+]},
+{path:'**',component:NotFoundComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,NotFoundComponent, HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todo/Notfound.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-Notfound',
+    template: '<h1> Page not found plz check url</h1>',
+   
+  })
+export class NotFoundComponent  {}
